019171b5be677eb3cdf2edb01f7d27f9a2a4d259
John Sag <johnsag@Johns-MacBook-Pro-2.local>
2020-10-09 15:32:36 -0400

    working select tag and added if statements

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
 const numberBills = document.querySelector('#number')
 const numberPeople = document.querySelector('#number-people')
-const isBirthday = document.getElementById("#is-birthday")
 const displayTotal = document.querySelector("#display-total")
-
 
 numberBills.addEventListener('input', calculate)
 numberPeople.addEventListener('input', calculate)
-
 // isBirthday.addEventListener('input', calculate)
 
-
-
-
-
-function calculate (){
+function calculate() {
     const billValue = numberBills.value
     const peopleValue = parseInt(numberPeople.value)
     // const monthValue = isBirthday.value
     const coins = 5
-
-
-
-
-
-
-
-
-
-
     const totalCoins = billValue * coins
     const peopleCoins = totalCoins / peopleValue
-
-    displayTotal.innerHTML= parseInt(peopleCoins)
-    
-
-
+    var x = document.getElementById("is-birthday").value;
+    if(x== "0"){
+        displayTotal.innerHTML= parseInt(peopleCoins)
+    }else if(x == "1"){
+       const birthdayTotal = peopleCoins + 5
+       displayTotal.innerHTML = parseInt(birthdayTotal)
+    }
+    if(x== "2"){
+        displayTotal.innerHTML= parseInt(peopleCoins)
+    }
 }
-
 calculate()
+
